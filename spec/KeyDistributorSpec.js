1427ccbf4f6c40d154c3a36136be5cad97a26a31
Chris Miller <cmille142@gmail.com>
2014-02-10 17:52:55 -0800

    fixed lint errors, random tweaks

---
+++ spec/KeyDistributorSpec.js
@@ -0,0 +1,126 @@
+/*global module, require */
+"use strict";
+
+var KeyDistributor = require('./../lib/KeyDistributor');
+
+module.exports = {
+
+  setUp : function (callback) {
+
+    this.defaultServer = {
+      host : '127.0.0.1',
+      port : 6379
+    };
+
+    this.keyDistributor = new KeyDistributor({
+        servers : [this.defaultServer]
+      }
+    );
+    callback();
+  },
+
+  tearDown : function (callback) {
+    this.keyDistributor = null;
+    callback();
+  },
+
+  testAddConnection : function (test) {
+    var options = this.defaultServer;
+    options.port = 6380;
+    var addResult = this.keyDistributor.addConnection(options, 1);
+    test.equal(this.keyDistributor, addResult);
+    test.done();
+  },
+
+  testAddDuplicateConnection : function (test) {
+    test.throws(
+      function () {
+        this.keyDistributor.addConnection(this.defaultServer, 1);
+      }.bind(this),
+      Error,
+      "Error: Connection" + JSON.stringify(this.defaultServer) + " already exists."
+    );
+
+    test.done();
+  },
+
+  testRemoveConnection : function (test) {
+    var removed = this.keyDistributor.removeConnection(this.defaultServer);
+    test.equal(this.keyDistributor, removed);
+
+
+    test.throws(
+      function () {
+
+        console.log("HEY");
+
+        this.keyDistributor.getConnectionByKeyName('test');
+
+      }.bind(this),
+      Error,
+      "Error: Connection" + this.defaultServer + " does not exist."
+    );
+
+    test.done();
+  },
+
+  testRemoveUnknownConnection : function (test) {
+    test.throws(
+      function () {
+        this.keyDistributor.removeConnection(this.defaultServer);
+        this.keyDistributor.removeConnection(this.defaultServer);
+      }.bind(this),
+      Error,
+      "Error: Connection" + this.defaultServer + "does not exist."
+    );
+
+    test.done();
+  },
+
+  testGetConnectionByKeyName : function (test) {
+    var connections = {};
+
+    var options = this.defaultServer;
+    options.port = 6380;
+    this.keyDistributor.addConnection(options, 1);
+
+    options.port = 6381;
+    this.keyDistributor.addConnection(options, 1);
+
+    options.port = 6382;
+    this.keyDistributor.addConnection(options, 1);
+
+    for (var i = 0; i < 5; i++) {
+      connections[i] = this.keyDistributor.getConnectionByKeyName("key_" + i);
+    }
+
+    var j = 0;
+    for (var connection in connections) {
+      var conn = this.keyDistributor.getConnectionByKeyName("key_" + j);
+      test.equal(connections[connection], conn);
+      j++;
+    }
+
+
+    test.done();
+  },
+
+  testMultipleGetConnectionByKeyName : function (test) {
+    var connections = {};
+
+    var options = this.defaultServer;
+    options.port = 6380;
+    this.keyDistributor.addConnection(options, 1);
+
+    options.port = 6381;
+    this.keyDistributor.addConnection(options, 1);
+
+    options.port = 6382;
+    this.keyDistributor.addConnection(options, 1);
+
+//    console.log("\n\nSHOULD BE 127.0.0.1:6380");
+//    console.log("\n" + this.keyDistributor.getConnectionByKeyName("provider:12345:members") + "\n");
+
+    test.done();
+  }
+};
